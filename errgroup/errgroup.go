6f9048ee64a4200f97627a888d07d44fa868051a
Tim Marston <tim.marston@lockwood-publishing.com>
2017-10-20 15:03:44 +0100

    added WaitWithTimeout() to errgroup

--- errgroup/errgroup.go
+++ errgroup/errgroup.go
@@ -7,7 +7,9 @@
 package errgroup
 
 import (
+	"errors"
 	"sync"
+	"time"
 
 	"golang.org/x/net/context"
 )
@@ -25,11 +27,13 @@ type Group struct {
 	err     error
 }
 
+var Timeout = errors.New("errgroup timed out")
+
 // WithContext returns a new Group and an associated Context derived from ctx.
 //
 // The derived Context is canceled the first time a function passed to Go
-// returns a non-nil error or the first time Wait returns, whichever occurs
-// first.
+// returns a non-nil error or the first time Wait or WaitWithTimeout returns,
+// whichever occurs first.
 func WithContext(ctx context.Context) (*Group, context.Context) {
 	ctx, cancel := context.WithCancel(ctx)
 	return &Group{cancel: cancel}, ctx
@@ -39,6 +43,30 @@ func WithContext(ctx context.Context) (*Group, context.Context) {
 // returns the first non-nil error (if any) from them.
 func (g *Group) Wait() error {
 	g.wg.Wait()
+	if g.cancel != nil {
+		g.cancel()
+	}
+	return g.err
+}
+
+// Wait blocks until all function calls from the Go method have returned, or
+// until the specified timeout has occurred, then returns the first non-nil
+// error (if any) from them.
+func (g *Group) WaitWithTimeout(timeout time.Duration) error {
+	done := make(chan interface{})
+
+	go func() {
+		defer close(done)
+		g.wg.Wait()
+	}()
+
+	select {
+	case <-done:
+	case <-time.After(timeout):
+		g.errOnce.Do(func() {
+			g.err = Timeout
+		})
+	}
 	if g.cancel != nil {
 		g.cancel()
 	}
